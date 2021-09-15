0d8be3ad95174391e5df2b4f21278d6622221172
ykh1214@naver.com <ykh1214@naver.com>
2021-09-15 23:20:42 +0900

    Feat : Add S05P21D106-171  S05P21D106-165_MoveMap Si Gu Dong , categoryFilter
    Former-commit-id: b1e53c572d3dad6a8583dcf331a01e5aca92b9db

--- frontend/src/layout/MainNavbar.vue
+++ frontend/src/layout/MainNavbar.vue
@@ -2,7 +2,7 @@
   <div>
     <DetailSearch v-show="isdetail" />
 
-    <navbar position="fixed" type="info" menu-classes="ml-auto">
+    <navbar position="fixed" type="info" menu-classes="ml-auto ">
       <template>
         <router-link v-popover:popover1 class="navbar-brand" to="/">
           나 혼자 살거다
@@ -21,11 +21,71 @@
       </template>
 
       <template slot="navbar-menu">
-        <li class="nav-item">
-          <a class="nav-link" @click="changeItem(0, 0)"> 상세 검색 </a>
+        <drop-down class="nav-item">
+          <n-button
+            slot="title"
+            type="primary"
+            class="dropdown-toggle"
+            data-toggle="dropdown"
+            block
+            round
+          >
+            {{ Si }}
+          </n-button>
+          <a
+            v-for="(items, index) in SiList"
+            :key="index"
+            class="dropdown-item"
+            @click="clickSi(items.name, index)"
+            >{{ items.name }}
+          </a>
+        </drop-down>
+
+        <drop-down class="nav-item">
+          <n-button
+            slot="title"
+            type="primary"
+            class="dropdown-toggle"
+            data-toggle="dropdown"
+            block
+            round
+          >
+            {{ Gu }}
+          </n-button>
+          <a
+            v-for="(items, index) in GuList[SiIdx].gu"
+            :key="index"
+            class="dropdown-item"
+            @click="clickGu(items.name, index)"
+            >{{ items.name }}
+          </a>
+        </drop-down>
+        <drop-down class="nav-item" style="margin-right: 100px">
+          <n-button
+            slot="title"
+            type="primary"
+            class="dropdown-toggle"
+            data-toggle="dropdown"
+            block
+            round
+          >
+            {{ Dong }}
+          </n-button>
+          <a
+            v-for="(items, index) in dongList[GuIdx].dong"
+            :key="index"
+            class="dropdown-item"
+            @click="clickDong(items.name, index)"
+            >{{ items.name }}
+          </a>
+        </drop-down>
+      </template>
+      <template slot="navbar-menu">
+        <li class="nav-item" style="margin-left: 80px">
+          <a class="nav-link mt-2" @click="changeItem()"> 상세 검색 </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link">
+          <a class="nav-link mt-2">
             <router-link to="/login">
               <i class="now-ui-icons media-1_button-power"></i>
               <p>로그인</p></router-link
@@ -33,7 +93,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link">
+          <a class="nav-link mt-2">
             <router-link to="/signup"><p>회원가입</p></router-link>
           </a>
         </li>
@@ -43,7 +103,7 @@
 </template>
 
 <script>
-import { DropDown, Navbar } from "@/components";
+import { DropDown, Navbar, Button } from "@/components";
 import { Popover } from "element-ui";
 import OneSearchBar from "../pages/SearchBar/OneSearchBar.vue";
 import ApartSearchBar from "../pages/SearchBar/ApartSearchBar.vue";
@@ -54,22 +114,80 @@ export default {
   props: {
     transparent: Boolean,
     colorOnScroll: Number,
-    
   },
   components: {
     DetailSearch,
     Navbar,
     [Popover.name]: Popover,
+    DropDown,
+    [Button.name]: Button,
   },
   data() {
     return {
+      SiIdx: 0,
+      GuIdx: 0,
+      DongIdx: 0,
+      Dong: "동 선택하세요",
+      Si: "시 선택하세요",
+      Gu: "구 선택하세요",
       isdetail: false,
       residenceType: "방 종류",
       residenceIndex: 0,
+      SiList: [{ name: "서울특별시" }, { name: "내가사는 천안시" }],
+      GuList: [
+        {
+          gu: [
+            { name: "용산구" },
+            { name: "강남구" },
+            { name: "강북구" },
+            { name: "서대문구" },
+          ],
+        },
+      ],
+      dongList: [
+        {
+          dong: [],
+        },
+        {
+          dong: [],
+        },
+        {
+          dong: [
+            { name: "삼양동" },
+            { name: "미아동" },
+            { name: "송천동" },
+            { name: "수유1동", lat: "37.5252081", long: "126.9300509" },
+          ],
+        },
+      ],
+      emitData: null,
     };
   },
   methods: {
-    clickSearch(name, index) {},
+    clickDong(dongName, idx) {
+      this.Dong = dongName;
+      this.DongIdx = idx;
+
+      let temp = this.Si + " " + this.Gu + " " + this.Dong;
+
+      const data = {
+        juso: temp,
+        lat: this.dongList[this.GuIdx].dong[this.DongIdx].lat,
+        long: this.dongList[this.GuIdx].dong[this.DongIdx].long,
+      };
+      this.emitData = data;
+      console.log("emit : ", this.emitData);
+      this.$emit("maker", this.emitData);
+    },
+
+    clickGu(guName, idx) {
+      this.Gu = guName;
+      this.GuIdx = idx;
+    },
+    clickSi(siName, idx) {
+      this.Si = siName;
+      this.SiIdx = idx;
+    },
 
     changeItem(a, index) {
       console.log(a, index);
--- frontend/src/pages/components/detailSearch.vue
+++ frontend/src/pages/components/detailSearch.vue
@@ -1,302 +1,309 @@
 <template>
   <div class="contaioner">
     <div class="col-md-12 wrap">
-      <div class="row lineTRL">
-        <div class="col-md-2">
-          <div class="category">방 유형</div>
-        </div>
-        <div class="row col-md-5">
-          <div class="col-md-4 lineL">
+      <div class="wraptemp">
+        <div class="row">
+          <div class="col-md-2">
+            <div class="category">방 유형</div>
+          </div>
+          <div class="row col-md-5">
+            <div class="col-md-4 lineL">
+              <n-checkbox
+                value="1"
+                v-model="checkedResiCategory[0].checkResi0"
+                @input="changeResiCategory"
+                >원룸</n-checkbox
+              >
+            </div>
+            <div class="col-md-4">
+              <n-checkbox
+                value="2"
+                v-model="checkedResiCategory[0].checkResi1"
+                @input="changeResiCategory"
+                >투 . 쓰리 룸</n-checkbox
+              >
+            </div>
+            <div class="col-md-4">
+              <n-checkbox
+                value="3"
+                v-model="checkedResiCategory[0].checkResi2"
+                @input="changeResiCategory"
+                >오피스텔</n-checkbox
+              >
+            </div>
+          </div>
+          <div class="col-md-5">
             <n-checkbox
-              value="1"
-              v-model="checkedResiCategory[0].checkResi0"
+              value="4"
+              v-model="checkedResiCategory[0].checkResi3"
               @input="changeResiCategory"
-              >원룸</n-checkbox
+              >아파트</n-checkbox
             >
           </div>
-          <div class="col-md-4">
+        </div>
+
+        <div class="row">
+          <div class="col-md-2 mt-auto mb-auto">
+            <div class="category">거래 유형</div>
+          </div>
+          <div class="col-md-3 lineL">
             <n-checkbox
-              value="2"
-              v-model="checkedResiCategory[0].checkResi1"
-              @input="changeResiCategory"
-              >투 . 쓰리 룸</n-checkbox
+              value="전세"
+              v-model="checkedResiType[0].TradingJ"
+              @input="changeResiType"
+              >전세</n-checkbox
             >
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3">
             <n-checkbox
-              value="3"
-              v-model="checkedResiCategory[0].checkResi2"
-              @input="changeResiCategory"
-              >오피스텔</n-checkbox
+              value="월세"
+              v-model="checkedResiType[0].TradingW"
+              @input="changeResiType"
+              >월세</n-checkbox
+            >
+          </div>
+          <div class="col-md-3">
+            <n-checkbox
+              value="매매"
+              v-model="checkedResiType[0].TradingM"
+              @input="changeResiType"
+              >매매</n-checkbox
             >
           </div>
         </div>
-        <div class="col-md-5 lineL">
-          <n-checkbox
-            value="4"
-            v-model="checkedResiCategory[0].checkResi3"
-            @input="changeResiCategory"
-            >아파트</n-checkbox
+
+        <div class="row">
+          <div class="col-md-2 mt-auto mb-auto">
+            <div class="category">가격</div>
+          </div>
+          <div class="col-md-3 lineL" v-show="checkedResiType[0].TradingJ">
+            <strong>보증금/전세가</strong>
+            <div class="row">
+              <div class="col-md-6 ml-auto mr-auto">
+                <p>{{ JrangeS }} ~</p>
+              </div>
+
+              <div class="col-md-6 end">
+                <p>{{ JrangeE }}</p>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <slider
+                class="slider-info mt-4"
+                v-model="rangeSlider"
+                :range="Jrange"
+                :connect="true"
+                @input="chaneJRange"
+                type="primary"
+              >
+              </slider>
+            </div>
+          </div>
+          <div class="col-md-3 lineL" v-show="!checkedResiType[0].TradingJ">
+            선택후 설정 해라
+          </div>
+          <div class="col-md-3" v-show="checkedResiType[0].TradingW">
+            <strong> 월세</strong>
+            <div class="row">
+              <div class="col-md-6 ml-auto mr-auto">
+                <p>{{ WrangeS }} ~</p>
+              </div>
+              <div class="col-md-6 end">
+                <p>{{ WrangeE }}</p>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <slider
+                class="slider-info mt-4"
+                v-model="dealMonthRange"
+                :range="Wrange"
+                :connect="true"
+                @input="changeWRange"
+                type="primary"
+              >
+              </slider>
+            </div>
+            <!--<Slider /> -->
+          </div>
+          <div class="col-md-3" v-show="!checkedResiType[0].TradingW">
+            선택후 설정 해라
+          </div>
+
+          <div class="col-md-3" v-show="checkedResiType[0].TradingM">
+            <strong> 매매가</strong>
+            <div class="row">
+              <div class="col-md-6 ml-auto mr-auto">
+                <p>{{ MrangeS }} ~</p>
+              </div>
+              <div class="col-md-6 end">
+                <p>{{ MrangeE }}</p>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <slider
+                class="slider-info mt-4"
+                v-model="dealTradingRange"
+                :range="Mrange"
+                :connect="true"
+                @input="changeMRange"
+                type="primary"
+              >
+              </slider>
+            </div>
+          </div>
+          <div class="col-md-3" v-show="!checkedResiType[0].TradingM">
+            선택후 설정 해라
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-2">
+            <div class="category">추가필터</div>
+          </div>
+          <div class="col-md-2 lineL">
+            <strong> 관리비</strong>
+            <div class="row">
+              <div class="col-md-6 ml-auto mr-auto">
+                <p>{{ ManageStart }} ~</p>
+              </div>
+              <div class="col-md-6 end">
+                <p>{{ ManageEnd }}</p>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <slider
+                class="slider-info mt-4"
+                v-model="costOfManage"
+                :connect="true"
+                :range="CofMrange"
+                @input="changeCM"
+                type="primary"
+              >
+              </slider>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <strong> 방크기</strong>
+            <div class="row">
+              <div class="col-md-6">
+                <p>{{ roomSizeStart }} ~</p>
+              </div>
+              <div class="col-md-6 end">
+                <p>{{ roomSizeEnd }}</p>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <slider
+                class="slider-info mt-4"
+                v-model="roomSize"
+                :connect="true"
+                :range="CofMrange"
+                @input="changeRoomsize"
+                type="primary"
+              >
+              </slider>
+            </div>
+          </div>
+
+          <div class="col-md-5" v-show="!checkedResiCategory[0].checkResi3">
+            <strong>층 수</strong>
+            <div class="row col-md-12">
+              <div class="col-md-2 floor">
+                <n-checkbox
+                  value="1층"
+                  v-model="checkedFloorType[0].floor1"
+                  @input="changeFloorType"
+                  >1층</n-checkbox
+                >
+              </div>
+              <div class="col-md-2 floor">
+                <n-checkbox
+                  value="2층"
+                  v-model="checkedFloorType[0].floor2"
+                  @input="changeFloorType"
+                  >2층</n-checkbox
+                >
+              </div>
+              <div class="col-md-2 floor">
+                <n-checkbox
+                  value="3층"
+                  v-model="checkedFloorType[0].floor3"
+                  @input="changeFloorType"
+                  >3층</n-checkbox
+                >
+              </div>
+              <div class="col-md-2 floor">
+                <n-checkbox
+                  value="4층"
+                  v-model="checkedFloorType[0].floor4"
+                  @input="changeFloorType"
+                  >4층</n-checkbox
+                >
+              </div>
+              <div class="col-md-3 floor">
+                <n-checkbox
+                  value="5층 이상"
+                  v-model="checkedFloorType[0].floor5"
+                  @input="changeFloorType"
+                  >5층 이상</n-checkbox
+                >
+              </div>
+            </div>
+            <strong>방 구조</strong>
+            <div class="row col-md-12">
+              <div class="col-md-3 floor">
+                <n-checkbox
+                  value="1.5룸"
+                  v-model="checkedRoomStruc[0].struct1"
+                  @input="changeStruct"
+                  >주방 분리형(1.5 룸)</n-checkbox
+                >
+              </div>
+              <div class="col-md-3 floor">
+                <n-checkbox
+                  value="복층"
+                  v-model="checkedRoomStruc[0].struct2"
+                  @input="changeStruct"
+                  >복층</n-checkbox
+                >
+              </div>
+              <div class="col-md-3 floor">
+                <n-checkbox
+                  value="투룸"
+                  v-model="checkedRoomStruc[0].struct3"
+                  @input="changeStruct"
+                  >투룸</n-checkbox
+                >
+              </div>
+              <div class="col-md-3 floor">
+                <n-checkbox
+                  value="쓰리룸 이상"
+                  v-model="checkedRoomStruc[0].struct4"
+                  @input="changeStruct"
+                  >쓰리룸 이상</n-checkbox
+                >
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-md-5 lineL"
+            v-show="checkedResiCategory[0].checkResi3"
           >
-        </div>
-      </div>
+            아파트 설정 불가
+          </div>
 
-      <div class="row lineTRL">
-        <div class="col-md-2 mt-auto mb-auto">
-          <div class="category">거래 유형</div>
-        </div>
-        <div class="col-md-3 lineL">
-          <n-checkbox
-            value="전세"
-            v-model="checkedResiType[0].TradingJ"
-            @input="changeResiType"
-            >전세</n-checkbox
-          >
-        </div>
-        <div class="col-md-3 lineL">
-          <n-checkbox
-            value="월세"
-            v-model="checkedResiType[0].TradingW"
-            @input="changeResiType"
-            >월세</n-checkbox
-          >
-        </div>
-        <div class="col-md-3 lineL">
-          <n-checkbox
-            value="매매"
-            v-model="checkedResiType[0].TradingM"
-            @input="changeResiType"
-            >매매</n-checkbox
-          >
-        </div>
-      </div>
-
-      <div class="row lineRL">
-        <div class="col-md-2 mt-auto mb-auto">
-          <div class="category">가격</div>
-        </div>
-        <div class="col-md-3 lineL" v-show="checkedResiType[0].TradingJ">
-          <strong>보증금/전세가</strong>
-          <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-              <p>{{ JrangeS }} ~</p>
-            </div>
-
-            <div class="col-md-6 end">
-              <p>{{ JrangeE }}</p>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <slider
-              class="slider-info mt-4"
-              v-model="rangeSlider"
-              :range="Jrange"
-              :connect="true"
-              @input="chaneJRange"
-              type="primary"
-            >
-            </slider>
-          </div>
-        </div>
-        <div class="col-md-3 lineL" v-show="!checkedResiType[0].TradingJ">
-          선택후 설정 해라
-        </div>
-        <div class="col-md-3 lineL" v-show="checkedResiType[0].TradingW">
-          <strong> 월세</strong>
-          <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-              <p>{{ WrangeS }} ~</p>
-            </div>
-            <div class="col-md-6 end">
-              <p>{{ WrangeE }}</p>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <slider
-              class="slider-info mt-4"
-              v-model="dealMonthRange"
-              :range="Wrange"
-              :connect="true"
-              @input="changeWRange"
-              type="primary"
-            >
-            </slider>
-          </div>
-          <!--<Slider /> -->
-        </div>
-        <div class="col-md-3 lineL" v-show="!checkedResiType[0].TradingW">
-          선택후 설정 해라
-        </div>
-
-        <div class="col-md-3 lineL" v-show="checkedResiType[0].TradingM">
-          <strong> 매매가</strong>
-          <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-              <p>{{ MrangeS }} ~</p>
-            </div>
-            <div class="col-md-6 end">
-              <p>{{ MrangeE }}</p>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <slider
-              class="slider-info mt-4"
-              v-model="dealTradingRange"
-              :range="Mrange"
-              :connect="true"
-              @input="changeMRange"
-              type="primary"
-            >
-            </slider>
-          </div>
-        </div>
-        <div class="col-md-3 lineL" v-show="!checkedResiType[0].TradingM">
-          선택후 설정 해라
-        </div>
-      </div>
-
-      <div class="row lineAll">
-        <div class="col-md-2">
-          <div class="category">추가필터</div>
-        </div>
-        <div class="col-md-2 lineL">
-          <strong> 관리비</strong>
-          <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-              <p>{{ ManageStart }} ~</p>
-            </div>
-            <div class="col-md-6 end">
-              <p>{{ ManageEnd }}</p>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <slider
-              class="slider-info mt-4"
-              v-model="costOfManage"
-              :connect="true"
-              :range="CofMrange"
-              @input="changeCM"
-              type="primary"
-            >
-            </slider>
-          </div>
-        </div>
-
-        <div class="col-md-2 lineL">
-          <strong> 방크기</strong>
-          <div class="row">
-            <div class="col-md-6">
-              <p>{{ roomSizeStart }} ~</p>
-            </div>
-            <div class="col-md-6 end">
-              <p>{{ roomSizeEnd }}</p>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <slider
-              class="slider-info mt-4"
-              v-model="roomSize"
-              :connect="true"
-              :range="CofMrange"
-              @input="changeRoomsize"
-              type="primary"
-            >
-            </slider>
-          </div>
-        </div>
-
-        <div class="col-md-5 lineL" v-show="!checkedResiCategory[0].checkResi3">
-          <strong>층 수</strong>
-          <div class="row col-md-12">
-            <div class="col-md-2 ml-auto floor">
-              <n-checkbox
-                value="1층"
-                v-model="checkedFloorType[0].floor1"
-                @input="changeFloorType"
-                >1층</n-checkbox
-              >
-            </div>
-            <div class="col-md-2 ml-auto floor">
-              <n-checkbox
-                value="2층"
-                v-model="checkedFloorType[0].floor2"
-                @input="changeFloorType"
-                >2층</n-checkbox
-              >
-            </div>
-            <div class="col-md-2 ml-auto floor">
-              <n-checkbox
-                value="3층"
-                v-model="checkedFloorType[0].floor3"
-                @input="changeFloorType"
-                >3층</n-checkbox
-              >
-            </div>
-            <div class="col-md-2 ml-auto floor">
-              <n-checkbox
-                value="4층"
-                v-model="checkedFloorType[0].floor4"
-                @input="changeFloorType"
-                >4층</n-checkbox
-              >
-            </div>
-            <div class="col-md-3 ml-auto floor">
-              <n-checkbox
-                value="5층 이상"
-                v-model="checkedFloorType[0].floor5"
-                @input="changeFloorType"
-                >5층 이상</n-checkbox
+          <div class="col-md-1">
+            <div class="col-md-11">
+              <a
+                class="btn btn-warning btn-round btn-block"
+                style="margin-top: 90px"
+                @click="setDetailSave"
+                >저장</a
               >
             </div>
           </div>
-          <strong>방 구조</strong>
-          <div class="row col-md-12">
-            <div class="col-md-3 ml-auto floor">
-              <n-checkbox
-                value="1.5룸"
-                v-model="checkedRoomStruc[0].struct1"
-                @input="changeStruct"
-                >주방 분리형(1.5 룸)</n-checkbox
-              >
-            </div>
-            <div class="col-md-3 ml-auto floor">
-              <n-checkbox
-                value="복층"
-                v-model="checkedRoomStruc[0].struct2"
-                @input="changeStruct"
-                >복층</n-checkbox
-              >
-            </div>
-            <div class="col-md-3 ml-auto floor">
-              <n-checkbox
-                value="투룸"
-                v-model="checkedRoomStruc[0].struct3"
-                @input="changeStruct"
-                >투룸</n-checkbox
-              >
-            </div>
-            <div class="col-md-3 ml-auto floor">
-              <n-checkbox
-                value="쓰리룸 이상"
-                v-model="checkedRoomStruc[0].struct4"
-                @input="changeStruct"
-                >쓰리룸 이상</n-checkbox
-              >
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-5 lineL" v-show="checkedResiCategory[0].checkResi3">
-          아파트 설정 불가
-        </div>
-
-        <div class="col-md-1">
-          <a
-            class="btn btn-warning btn-round btn-block"
-            style="margin-top: 100px"
-            @click="setDetailSave"
-            >저장</a
-          >
         </div>
       </div>
     </div>
@@ -630,17 +637,24 @@ export default {
         startArea: this.roomSize[0],
         endArea: this.roomSize[0] == 50 ? 0 : this.roomSize[1],
       };
-    }
+    },
   },
   props: {},
 };
 </script>
 
 <style scoped>
+.wraptemp {
+  z-index: 4;
+
+  background-color: rgb(255, 255, 255);
+  border-radius: 20px;
+  box-shadow: 0 5px 18px -7px rgb(85, 83, 83);
+}
 .wrap {
-  z-index: 999;
-  margin-top: 47px;
-  background-color: white;
+  padding-bottom: 25px;
+  z-index: 3;
+  margin-top: 55px;
 }
 .category {
   color: black;
@@ -661,18 +675,18 @@ export default {
   text-align: bottom;
 }
 .lineAll {
-  border: 1px solid black;
+  border: 1px solid #eb8816;
 }
 .lineTRL {
-  border-top: 1px solid black;
-  border-right: 1px solid black;
-  border-left: 1px solid black;
+  border-top: 1px solid #eb8816;
+  border-right: 1px solid #eb8816;
+  border-left: 1px solid #eb8816;
 }
 .lineRL {
-  border-right: 1px solid black;
-  border-left: 1px solid black;
+  border-right: 1px solid #eb8816;
+  border-left: 1px solid #eb8816;
 }
 .lineL {
-  border-left: 1px solid black;
+  border-left: 1px solid #eb8816;
 }
 </style>
