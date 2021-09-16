2672aa4a27ed9f175b0e6d783b10cf21416d5f77
bongdaehyun <pkjeogus@gmail.com>
2021-09-16 22:58:55 +0900

    Test : no-console test1
    Former-commit-id: 3ea6fe9a4fa0ba5f4ca7150710ebc1ca07ffd90e

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
   },
   extends: ['plugin:vue/essential'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    
+    'no-console': 'off',
+    //'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
