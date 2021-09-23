c80987e0a2a2ccaca10b826e95142eacd6c037d5
ykh1214@naver.com <ykh1214@naver.com>
2021-09-23 14:27:21 +0900

    Fix : S05P21D106-66 QnApage Bugfix
    Former-commit-id: d0b5beae331c6b8381aaba2f0a4b3c61d7f9615f

--- frontend/src/pages/components/newQnAProgress.vue
+++ frontend/src/pages/components/newQnAProgress.vue
@@ -1,12 +1,9 @@
 <template>
   <div id="app">
-    <card
-      class="card-nav-tabs text-center"
-      header-classes="card-header-warning"
-    >
+    <card>
       <div slot="header" class="col-md-12 my-3">
         <h4>
-          <n-progress class="my-4" type="primary" :value="pr" show-value>
+          <n-progress class="my-4" type="warning" :value="pr" show-value>
           </n-progress>
           <strong> {{ QnA[index].question }}</strong>
         </h4>
@@ -20,7 +17,7 @@
             ml-auto
             mr-auto
             my-4
-            btn btn-primary btn-round btn-lg btn-block
+            btn btn-warning btn-round btn-lg btn-block
           "
           :title="areatitle"
         >
@@ -42,7 +39,7 @@
             mr-auto
             my-4
             select
-            btn btn-primary btn-round btn-lg btn-block
+            btn btn-warning btn-round btn-lg btn-block
             my-4
           "
           @click="next(areatitle, areaindex)"
@@ -63,7 +60,7 @@
             ml-auto
             my-4
             mr-auto
-            btn btn-primary btn-round btn-lg btn-block
+            btn btn-warning btn-round btn-lg btn-block
           "
           >{{ a }}
         </a>
@@ -108,7 +105,7 @@ export default {
       //console.log(ans);
 
       if (this.index == 4) {
-       // console.log(this.QnARes);
+        // console.log(this.QnARes);
         this.setRes(this.QnARes);
         return;
       }
