48659cee3e70f449334ee883388a7e54d93e1b28
bongdaehyun <pkjeogus@gmail.com>
2021-10-08 03:04:23 +0900

    Push
    Former-commit-id: 229ebfe9f4187cc200f25d526c09e327493dd236

--- frontend/src/pages/map/ResidenceList.vue
+++ frontend/src/pages/map/ResidenceList.vue
@@ -227,7 +227,6 @@ export default {
         this.sortArea = true;
         this.sortLike = true;
         this.clickCost = true;
-        //push event
         this.clickArea = false;
         this.clickLike = false;
       } else if (this.sortFilter == "area") {
